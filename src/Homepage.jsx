f7146b0a204b111fd69525011ae047d21869fc7e
iMac7 <macharian77@gmail.com>
2022-01-24 17:34:01 +0300

    nav

---
+++ src/Homepage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Sidenav from './Sidenav'
+import Home from './Home'
+import Trends from './Trends'
+
+function Homepage() {
+    return (
+        <>
+        <Sidenav/>
+        <Home/>
+        <Trends/>
+        </>
+    )
+}
+
+export default Homepage
+
---
+++ src/Sidenav.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Sidenav() {
+    return (
+        <nav>
+            sidenav
+        </nav>
+    )
+}
+
+export default Sidenav
