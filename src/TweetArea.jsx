95e5c0511bb3efcebcc887cc47d791dce04cfcca
iMac7 <macharian77@gmail.com>
2022-01-24 21:19:55 +0300

    committed more files

---
+++ src/TweetArea.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TweetArea() {
+    return (
+        <div>
+            tweetarea
+        </div>
+    )
+}
+
+export default TweetArea
---
+++ src/Tweets.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Tweets() {
+    return (
+        <div>
+            tweets go here
+        </div>
+    )
+}
+
+export default Tweets
