dc67dad975f44e53d1386ed722e82928fdcb2f0d
iMac7 <macharian77@gmail.com>
2022-01-25 07:32:32 +0300

    connected to backend

--- src/Home/TweetArea.jsx
+++ src/Home/TweetArea.jsx
@@ -1,14 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Avatar, Button } from '@mui/material'
 import './tweetArea.css'
 
 function TweetArea() {
+
+    const [inputValue, setInputValue] = useState('')
+
+    const handleClick =(e)=>{
+        e.preventDefault()
+
+        fetch("http://localhost:3001/",{
+            method: 'POST',
+            headers : {
+            'Content-Type' : 'application/json'
+          },
+          body:JSON.stringify(
+            {name:inputValue}
+          )
+        })
+
+        setInputValue('')
+      
+    }
+
     return (
          <div id="tweetBox">
             <Avatar className='avatar'/>
             <form id='tweetForm'>
-                <input type="text" id='tweetInput' />
-                <Button>Tweet</Button>
+                <input type="text" id='tweetInput' value={inputValue}
+                onChange={e=> setInputValue(e.target.value)} />
+                <Button onClick={handleClick}>Tweet</Button>
             </form>
          </div>
     )
