cbfe39511a1293642b580cb0bae70c398611131b
iMac7 <macharian77@gmail.com>
2022-05-12 18:49:14 +0300

    build folder

---
+++ build/static/js/748.3bbe9073.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkchat_frontend=self.webpackChunkchat_frontend||[]).push([[748],{748:function(e,n,t){t.r(n),n.default={}}}]);
+//# sourceMappingURL=748.3bbe9073.chunk.js.map
